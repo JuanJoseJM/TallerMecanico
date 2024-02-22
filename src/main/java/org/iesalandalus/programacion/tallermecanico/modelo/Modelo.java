cbd7335f2f44432244186cf77aa006a339e9db2c
JuanJoseJM <juanjosejuarezmensales@gail.com>
2024-02-22 13:50:55 +0100

    Nombre de alumno en README.md hecho
    --Dominio--
    Cliente.java creado y comprobado correctamente
    Vehiculo.java creado y comprobado correctamente
    Revision.Java creado y comprobado correctamente
    
    --Negocio--
    Clientes.java creado y comprobado correctamente
    Vehiculos.java creado y comprobado correctamente
    Revisiones.java creado y comprobado correctamente
    
    Modelo.java creado y comprobado correctamente
    
    --Vista--
    Opcion.java creado y comprobado correctamente
    Consola.java creado y comprobado correctamente
    Vista.java creado y comprobado correctamente
    
    --Controlador--
    Controlador.java creado
    
    --Main creado

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/Modelo.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/Modelo.java
@@ -23,56 +23,70 @@ public class Modelo {
         vehiculos = new Vehiculos();
         revisiones = new Revisiones();
     }
+
     public void terminar(){
         System.out.println("Fin");
     }
+
     public void insertar(Cliente cliente) throws OperationNotSupportedException {
         clientes.insertar(new Cliente(cliente));
     }
+
     public void insertar(Vehiculo vehiculo) throws OperationNotSupportedException {
         vehiculos.insertar(vehiculo);
     }
+
     public void insertar(Revision revision) throws OperationNotSupportedException {
         Cliente cliente = clientes.buscar(revision.getCliente());
         Vehiculo vehiculo = vehiculos.buscar(revision.getVehiculo());
         revisiones.insertar(new Revision(cliente,vehiculo,revision.getFechaInicio()));
     }
+
     public Cliente buscar(Cliente cliente){
        // return new clientes.buscar(cliente);
         cliente = Objects.requireNonNull(clientes.buscar(cliente));
         return new Cliente(cliente);
     }
+
     public Vehiculo buscar(Vehiculo vehiculo){
         return vehiculos.buscar(vehiculo);
     }
+
     public Revision buscar(Revision revision){
         return revisiones.buscar(revision);
 
     }
+
     public boolean modificar(Cliente cliente,String nombre, String telefono) throws OperationNotSupportedException {
         return clientes.modificar(cliente,nombre,telefono);
     }
+
     public void anadirHoras(Revision revision, int horas) throws OperationNotSupportedException {
         revisiones.anadirHoras(revision,horas);
     }
+
     public void anadirPrecioMaterial(Revision revision, float precioMaterial) throws OperationNotSupportedException {
         revisiones.anadirPrecioMaterial(revision,precioMaterial);
     }
+
     public void cerrar(Revision revision, LocalDate fechaFin) throws OperationNotSupportedException {
         revisiones.cerrar(revision,fechaFin);
     }
+
     public void borrar(Cliente cliente) throws OperationNotSupportedException {
         for (Revision revision: revisiones.get(cliente)){
             revisiones.borrar(revision);
         }
         clientes.borrar(cliente);
     }
+
     public void borrar(Vehiculo vehiculo) throws OperationNotSupportedException {
         for (Revision revision: revisiones.get(vehiculo)){
             revisiones.borrar(revision);
         }
         vehiculos.borrar(vehiculo);
     }
+
     public void borrar(Revision revision) throws OperationNotSupportedException {
         revisiones.borrar(revision);
     }
@@ -87,7 +101,6 @@ public class Modelo {
         return new ArrayList<>(vehiculos.get());
     }
 
-
     public List<Revision> getRevisiones(){
         ArrayList<Revision> listaRevisones = new ArrayList<>();
         for (Revision revision : revisiones.get()) {
@@ -95,14 +108,15 @@ public class Modelo {
         }
         return listaRevisones;
     }
+
     public List<Revision> getRevisiones(Cliente cliente){
         ArrayList<Revision> listaRevisonesIgualCliente = new ArrayList<>();
         for (Revision revision : revisiones.get(cliente)) {
             listaRevisonesIgualCliente.add(new Revision (revision));
         }
-
         return listaRevisonesIgualCliente;
     }
+
     public List<Revision> getRevisiones(Vehiculo vehiculo){
         ArrayList<Revision> listaRevisonesIgualVehiculo = new ArrayList<>();
         for (Revision revision : revisiones.get(vehiculo)) {
